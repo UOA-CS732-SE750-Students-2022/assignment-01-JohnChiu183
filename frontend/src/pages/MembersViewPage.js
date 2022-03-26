3df1ba453edf6ff654cf00d26a699d7184ad53a5
John Chiu <hchi183@aucklanduni.ac.nz>
2022-03-26 23:35:29 +0800

    Enable colorful hashtags

--- frontend/src/pages/MembersViewPage.js
+++ frontend/src/pages/MembersViewPage.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import '../App.css';
 import '../AppContextProvider';
 import { AppContext } from '../AppContextProvider';
-import { Card, List } from 'antd';
+import { Card, List, Tag, Divider } from 'antd';
 import { SettingOutlined } from '@ant-design/icons';
 
 export default function MemberViewPage(){
@@ -12,18 +12,19 @@ export default function MemberViewPage(){
 
     // Key: renderItem based on dataSource can reduce coding effort when looping data  
     return(
-        <List grid={{gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3}}
+        <List grid={{gutter: 16, xs: 1, sm: 2, md: 3, lg: 3, xl: 4, xxl: 6}}
                 dataSource={memberList}
                 renderItem={item =>(
                     <List.Item>
-                        <Card hoverable actions={[<SettingOutlined key="setting" />]}>
+                        <Card style={{textAlign: 'center'}} hoverable actions={[<SettingOutlined key="setting" />]}>
                             <Meta
                                 title={item.LastName + ' ' + item.FirstName}  
                                 description={
                                     <div>
                                         <p>{item.CompanyName + '-' + item.Position} <br/>
                                             {item.Email} <br/>
-                                            {item.HashTag}
+                                            <Divider orientation="center" style={{fontSize: 'smaller'}}>Member HashTags</Divider>
+                                            {item.HashTag.map((tag) => <Tag color="blue">{tag}</Tag>)}
                                         </p>
                                     </div>}/>
                         </Card>
